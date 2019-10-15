7c359e01451b2d7f099e129fff7fb7a198a9cb03
rchurchASC <rahmelchurch@gmail.com>
2019-10-15 22:25:49 +0000

    first

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $( "document" ).ready(function() {
+    $("#button").click(function(){
+        let word = $(".input").val();
+        let letters = word.split(" ");
+        if (letters = "a") {
 
-
-
+        }
+    });
 });
