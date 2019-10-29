1e3bb7102bf77fce026f8ea15d94c439426b17ff
rchurchASC <rahmelchurch@gmail.com>
2019-10-29 21:51:44 +0000

    Enter

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 $( "document" ).ready(function() {
+    $(".container").keyup(function (event) {
+        if (event.keyCode === 13) {
+            $("#button").click();
+        }
+    });
     $("#button").click(function(){
         let word = $(".input").val();
         let letters = word.split('');
-        if (word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u") {
+        if (word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u") {
             letters.push("ay");
         }else{
             letters.push(word[0]);
             letters.shift();
             letters.push("ay");
-            console.log(letters);
         }
         $(".output").text(letters.join(''));
     });
